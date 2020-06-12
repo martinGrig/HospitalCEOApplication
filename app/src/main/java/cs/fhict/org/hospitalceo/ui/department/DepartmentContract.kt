6615cf1cdca1b1a0182b19d8005a35a4a8fb989b
David Petrov <david.petrov@student.fontys.nl>
2020-06-12 10:32:03 +0300

    Learning contract and presenter

--- app/src/main/java/cs/fhict/org/hospitalceo/ui/department/DepartmentContract.kt
+++ app/src/main/java/cs/fhict/org/hospitalceo/ui/department/DepartmentContract.kt
@@ -1,3 +1,23 @@
 package cs.fhict.org.hospitalceo.ui.department
 
-interface DepartmentContract
+import cs.fhict.org.hospitalceo.data.model.Bed
+import cs.fhict.org.hospitalceo.utils.mvp.IBaseActivity
+import cs.fhict.org.hospitalceo.utils.mvp.IBaseFragment
+import cs.fhict.org.hospitalceo.utils.mvp.IBasePresenter
+
+interface DepartmentContract {
+    interface View : IBaseActivity{
+
+    }
+    interface ItemView {
+        fun bindItem(item: Bed)
+    }
+
+    interface Presenter : IBasePresenter<View>{
+        //counter of items
+        val itemCount: Int
+        //starts a new Activity when item clicked
+        fun onItemClicked(pos: Int)
+        fun onBindItemView(itemView: ItemView, pos: Int)
+    }
+}
--- app/src/main/java/cs/fhict/org/hospitalceo/ui/department/DepartmentPresenter.kt
+++ app/src/main/java/cs/fhict/org/hospitalceo/ui/department/DepartmentPresenter.kt
@@ -1,3 +1,30 @@
 package cs.fhict.org.hospitalceo.ui.department
 
-class DepartmentPresenter
+import cs.fhict.org.hospitalceo.data.BedsRepository
+import cs.fhict.org.hospitalceo.data.model.Bed
+
+
+class DepartmentPresenter(bedRep: BedsRepository) : DepartmentContract.Presenter {
+    var view: DepartmentContract.View? = null;
+    var beds = ArrayList<Bed>() // can be done with interactor
+    var bedRep: BedsRepository = bedRep
+
+    override val itemCount: Int
+        get() = beds?.size!!
+
+    override fun onItemClicked(pos: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindItemView(itemView: DepartmentContract.ItemView, pos: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onViewActive(view: DepartmentContract.View) {
+        this.view = view
+    }
+
+    override fun onViewInactive() {
+        this.view = null
+    }
+}
